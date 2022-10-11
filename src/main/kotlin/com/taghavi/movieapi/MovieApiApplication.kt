9f3fd059c1a1743523e33b61ddbecceb7746f6c9
EhsanTgv <EhsanTgv@gmail.com>
2022-10-11 11:24:02 +0300

    Sample endpoint

--- src/main/kotlin/com/taghavi/movieapi/MovieApiApplication.kt
+++ src/main/kotlin/com/taghavi/movieapi/MovieApiApplication.kt
@@ -2,9 +2,16 @@ package com.taghavi.movieapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
+@RestController
 @SpringBootApplication
-class MovieApiApplication
+class MovieApiApplication{
+
+	@GetMapping
+	fun helloWorld()="Hello World! From Spring"
+}
 
 fun main(args: Array<String>) {
 	runApplication<MovieApiApplication>(*args)
